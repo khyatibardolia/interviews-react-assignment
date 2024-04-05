96802fc3ca26df2b5cc03c60605af07704b3af9f
Khyati Bardolia <khyati.bardolia@gmail.com>
2024-04-05 01:25:30 -0400

    chore: add suspense component to dislay fallback ui when components are loading

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,14 +1,18 @@
 import { Route, Routes } from 'react-router-dom';
-import React from "react";
+import React, { Suspense } from "react";
+import {Box, CircularProgress} from "@mui/material";
 
 const ProductsComponent = React.lazy(() => import('../components/Products/Products'));
 const CheckoutStepperComponent = React.lazy(() => import('../components/checkout/stepper/CheckoutStepper'));
 
 export const AppRoutes = () => {
     return (
-        <Routes>
-            <Route path="/" element={<ProductsComponent />} />
-            <Route path="/checkout" element={<CheckoutStepperComponent />} />
-        </Routes>
+        <Suspense fallback={<Box sx={{display: 'flex', justifyContent: 'center', marginTop: '30px'}}>
+            <CircularProgress size={75} /></Box>}>
+            <Routes>
+                <Route path="/" element={<ProductsComponent />} />
+                <Route path="/checkout" element={<CheckoutStepperComponent />} />
+            </Routes>
+        </Suspense>
     );
 };
